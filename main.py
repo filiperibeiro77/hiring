c0d16b40ff3c57454c9727c83393152677ba4e5c
Filipe Ribeiro <filiperibeirom7@gmail.com>
2018-01-10 21:55:52 -0200

    adding test
    Signed-off-by: Filipe Ribeiro <filiperibeirom7@gmail.com>

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+import unittest
 from block_io import BlockIo
 import sys
+import requests
 
-version = 2
-block_io = BlockIo('e61b-2513-431a-3906', 'shakurraph2', version)
+class Main:
 
-def send_bitcoin():
+    def send_bitcoin(self):
+        version = 2
+        block_io = BlockIo('e61b-2513-431a-3906', 'shakurraph2', version)
         block_io.withdraw_from_labels(
-        amounts='0.001',
-        from_labels='default',
-        to_addresses='2N23YsTMNSdgxcfebqekGcJjDEkqKk8eunm',
-        pin='shakurraph2'
-    )
+            amounts='0.001',
+            from_labels='default',
+            to_addresses='2N23YsTMNSdgxcfebqekGcJjDEkqKk8eunm',
+            pin='shakurraph2'
+        )
 
-if __name__ == "__main__":
-    print(send_bitcoin())
+
+class MainTest(unittest.TestCase):
+    def testMain(self):
+        result = Main().send_bitcoin()
+        self.assertEqual(result["status"], "succes")
+
+
+if __name__ == '__main__':
+    unittest.main()
